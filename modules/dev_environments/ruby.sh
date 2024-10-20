5d30d25a190a2c6dd1bc4c025792444ffe323c73
Vincent Koc <vincentkoc@ieee.org>
2024-10-20 23:36:05 +1100

    Update ruby.sh

--- modules/dev_environments/ruby.sh
+++ modules/dev_environments/ruby.sh
@@ -8,27 +8,33 @@ log_info "Setting up Ruby environment..."
 if ! command -v rbenv &> /dev/null; then
     log_info "rbenv not found. Installing rbenv..."
     brew install rbenv
+
+    # Initialize rbenv
+    export PATH="$HOME/.rbenv/bin:$PATH"
+    eval "$(rbenv init -)"
+
+    # Install ruby-build as an rbenv plugin
+    if [ ! -d "$(rbenv root)/plugins/ruby-build" ]; then
+        log_info "Installing ruby-build plugin for rbenv..."
+        git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
+    fi
+
+    # Install rbenv-default-gems as an rbenv plugin
+    if [ ! -d "$(rbenv root)/plugins/rbenv-default-gems" ]; then
+        log_info "Installing rbenv-default-gems plugin for rbenv..."
+        git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)/plugins/rbenv-default-gems"
+    fi
+
+    # Create a default-gems file with default gems to install
+    log_info "Creating default-gems file..."
+    cat << EOF > "$(rbenv root)/default-gems"
+bundler
+EOF
+else
+    # Initialize rbenv
+    export PATH="$HOME/.rbenv/bin:$PATH"
     eval "$(rbenv init -)"
 fi
-
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
-
-# Install ruby-build as an rbenv plugin
-if [ ! -d "$(rbenv root)/plugins/ruby-build" ]; then
-    log_info "Installing ruby-build plugin for rbenv..."
-    git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)/plugins/ruby-build"
-fi
-
-# Install rbenv-default-gems as an rbenv plugin
-if [ ! -d "$(rbenv root)/plugins/rbenv-default-gems" ]; then
-    log_info "Installing rbenv-default-gems plugin for rbenv..."
-    git clone https://github.com/rbenv/rbenv-default-gems.git "$(rbenv root)/plugins/rbenv-default-gems"
-fi
-
-# Create a default-gems file
-log_info "Creating default-gems file..."
-echo "bundler" > "$(rbenv root)/default-gems"
 
 # Check if desired Ruby version is installed
 CURRENTVER=$(get_current_version rbenv)
@@ -46,32 +52,41 @@ if [ "$INSTALLED" = true ]; then
     log_info "Skipping installation of Ruby."
     ruby --version | tee -a "$LOGFILE"
     which ruby | tee -a "$LOGFILE"
-
 else
     log_warning "Ruby [$RUBYVER] is not installed. Found [$CURRENTVER]."
-    log_info "Installing Ruby..."
+    log_info "Installing Ruby [$RUBYVER]..."
     rbenv install "$RUBYVER" | tee -a "$LOGFILE"
 
-    # Set RUBYVER as the local and global Ruby version
+    # Set RUBYVER as the global Ruby version
     rbenv global "$RUBYVER"
-    rbenv local "$RUBYVER"
 
     # If there are other versions installed, set the highest one as the global version
     HIGHESTVER=$(get_highest_version rbenv)
     if [ "$HIGHESTVER" != "$RUBYVER" ]; then
         rbenv global "$HIGHESTVER"
+        log_info "Set highest Ruby version [$HIGHESTVER] as global version."
     fi
+
+    # Rehash rbenv shims
+    rbenv rehash
 
     # Show the active Ruby version
     ruby --version | tee -a "$LOGFILE"
 
     # Update the RubyGems system software to the latest version
     log_info "Updating RubyGems..."
-    gem update --system
+    gem update --system | tee -a "$LOGFILE"
 
     # Update all installed gems to their latest versions
     log_info "Updating installed gems..."
-    gem update
+    gem update | tee -a "$LOGFILE"
+
+    # Install Bundler if not already installed
+    if ! gem list bundler -i > /dev/null; then
+        log_info "Installing Bundler..."
+        gem install bundler | tee -a "$LOGFILE"
+        rbenv rehash
+    fi
 
     # Use rbenv-doctor to check the setup
     log_info "Running rbenv-doctor to verify the setup..."
