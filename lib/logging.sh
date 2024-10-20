0759c943b408eff5b0d4990012ecb2ac007ddcd8
Vincent Koc <vincentkoc@ieee.org>
2024-10-21 00:06:23 +1100

    Update logging.sh

--- lib/logging.sh
+++ lib/logging.sh
@@ -19,18 +19,22 @@
 
 # Logging Functions
 
+get_timestamp() {
+    date +"%Y-%m-%d %H:%M:%S"
+}
+
 log_info() {
-    echo -e "\033[0;36m$1\033[0m" | tee -a "$LOGFILE"
+    echo -e "$(get_timestamp) [INFO] \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_success() {
-    echo -e "\033[0;32m[ ✓ ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
+    echo -e "$(get_timestamp) \033[0;32m[SUCCESS]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_warning() {
-    echo -e "\033[0;33m[ ! ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
+    echo -e "$(get_timestamp) \033[0;33m[WARNING]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_error() {
-    echo -e "\033[0;31m[ ✗ ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
+    echo -e "$(get_timestamp) \033[0;31m[ERROR]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
