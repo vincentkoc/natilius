4f33074a2fcc7a8278f4bc8d208779bb24bca546
Vincent Koc <vincentkoc@ieee.org>
2024-10-20 23:17:56 +1100

    Update logging.sh

--- lib/logging.sh
+++ lib/logging.sh
@@ -3,17 +3,17 @@
 # Logging Functions
 
 log_info() {
-    echo -e "\033[0;36m$1\033[0m"
+    echo -e "\033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_success() {
-    echo -e "\033[0;32m[ ✓ ]\033[0m \033[0;36m$1\033[0m"
+    echo -e "\033[0;32m[ ✓ ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_warning() {
-    echo -e "\033[0;33m[ ! ]\033[0m \033[0;36m$1\033[0m"
+    echo -e "\033[0;33m[ ! ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
 
 log_error() {
-    echo -e "\033[0;31m[ ✗ ]\033[0m \033[0;36m$1\033[0m"
+    echo -e "\033[0;31m[ ✗ ]\033[0m \033[0;36m$1\033[0m" | tee -a "$LOGFILE"
 }
